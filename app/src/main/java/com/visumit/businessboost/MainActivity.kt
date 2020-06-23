b38e65c0dad1e2d9c7d92d9a4c606716e76398f8
“WesleyMeneghini” <wesley.meneghini@outlook.com>
2020-06-23 16:11:19 -0300

    start project

---
+++ app/src/main/java/com/visumit/businessboost/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.visumit.businessboost
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
