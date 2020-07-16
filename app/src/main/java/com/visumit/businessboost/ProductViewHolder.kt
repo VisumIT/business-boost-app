38fe08328b97a4cbb2362bc71c3da07d2f75cb33
“WesleyMeneghini” <wesley.meneghini@outlook.com>
2020-07-16 16:57:59 -0300

    pegando a imagem da api e colocando na recyclerView usando Picasso

--- app/src/main/java/com/visumit/businessboost/ProductViewHolder.kt
+++ app/src/main/java/com/visumit/businessboost/ProductViewHolder.kt
@@ -2,8 +2,10 @@ package com.visumit.businessboost
 
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import com.visumit.businessboost.model.Product
 import java.lang.String.format
 import java.text.DecimalFormat
@@ -14,8 +16,16 @@ class ProductViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClic
     private val textViewName = itemView.findViewById<TextView>(R.id.product_name)
     private val textViewPriceDiscount = itemView.findViewById<TextView>(R.id.product_price_discount)
     private val btnAddProduct = itemView.findViewById<Button>(R.id.btn_add_product)
+    private  val imageProduct: ImageView = itemView.findViewById(R.id.product_img)
 
     fun bind(item: Product) {
+        if (item.imagesUrl != "" && item.imagesUrl != null){
+            Picasso.get().load(item.imagesUrl).into(imageProduct)
+
+        }else{
+            Picasso.get().load(R.mipmap.boost_icon)
+
+        }
 
         textViewPrice.text = "R$ " + item.price.toString()
         textViewName.text = item.name.toString()
